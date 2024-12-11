284d36d314adb38e60bd8778c3459bbfd0ba0452
romaJoel <romanjoelortiz@gmail.com>
2024-12-11 08:15:06 -0300

    primer commit

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+    
+//BASE DE DATOS LOCAL        
+$dbhost = 'xxxxxxx';
+$dbuser = 'xxxxx';
+$dbpass = 'xxxxxx';
+$dbname = 'xxxxxxxxxx';
+
+//DATOS MAIL  (Reemplazar por mail origen)
+$user_mail = 'xxxxxxxxxxx@xxxxxxx.com';
+$pass_mail = 'xxxxxxxxxxxxxxx';
+$host_mail = 'smtp.gmail.com';
+$destino_mail = 'xxxxxxxxxxx@xxxxxxx.com';
+
+?>
